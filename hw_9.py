957da12491f8f98c8a2967ccf8ab5fc8cd707604
vadoomz <vadymvsmarchenko@gmail.com>
2023-01-22 16:58:36 +0200

    Homework_6_n

--- hw_9.py
+++ hw_9.py
@@ -5,39 +5,34 @@ Enter your command: ''')
 if enter == 'add':
   name = input('Enter name to add: ')
   phone = input('Enter phone to add: ')
-  with open(r'C:\Users\vadym\OneDrive\test2.TXT') as handle:
-      data = handle.read()
-  js = json.loads(data)
-  phone_record = js.setdefault(name, phone)
-  with open(r'C:\Users\vadym\OneDrive\test2.TXT', 'w') as f:
+  with open('Newj.JSON','r+') as handle:
+    js = json.load(handle)
+    phone_record = js.setdefault(name, phone)
+  with open('Newj.JSON','w') as f:
       json.dump(js, f)
 elif enter == 'stats':
-    with open(r'C:\Users\vadym\OneDrive\test2.TXT') as handle:
-        data = handle.read()
-        js = json.loads(data)
+    with open('Newj.JSON', 'r+') as handle:
+        js = json.load(handle)
         print(js.values())
 elif enter == 'delete':
     delete = input('Enter name you want to delete: ')
-    with open(r'C:\Users\vadym\OneDrive\test2.TXT') as handle:
-        data = handle.read()
-    js = json.loads(data)
+    with open('Newj.JSON', 'r+') as handle:
+        js = json.load(handle)
     if delete in js:
         del js[delete]
-        with open(r'C:\Users\vadym\OneDrive\test2.TXT', 'w') as f:
+        with open('Newj.JSON','w') as f:
             json.dump(js, f)
         print(f'Contact {delete} was successfully deleted')
     else:
         print('Sorry, there is no such contact here')
 elif enter == 'list':
-    with open(r'C:\Users\vadym\OneDrive\test2.TXT') as handle:
-        data = handle.read()
-    js = json.loads(data)
+    with open('Newj.JSON', 'r+') as handle:
+        js = json.load(handle)
     print(js.keys())
 elif enter == 'show':
     show = input('Find your contact by name: ')
-    with open(r'C:\Users\vadym\OneDrive\test2.TXT') as handle:
-        data = handle.read()
-    js = json.loads(data)
+    with open('Newj.JSON', 'r+') as handle:
+        js = json.load(handle)
     print(js.get(show, 'Sorry, there is no such contact here'))
 else:
     print('You entered something unexpected. Try again')
@@ -49,3 +44,9 @@ else:
 # 2. Написати декоратор, який буде записувати в файл назву функції, яку він декорує, і писати час її виклику.
 #
 # 3. В попередньо написаний кастомний Exception додати запис помилки і час її виникнення у файл.
+class MyCustomException(Exception):
+    pass
+try:
+  raise MyCustomException('Custom exception is occured')
+except Exception as error:
+  print(error)
