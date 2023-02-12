b3bc258dc2a6b6d38006c612961ae970d308e177
vadoomz <vadymvsmarchenko@gmail.com>
2023-02-12 17:44:17 +0200

    add_try_except

--- hw_9.py
+++ hw_9.py
@@ -1,41 +1,36 @@
 # 1. Використати файл як базу даних для збереження записів телефонної книги із попередніх завдань.
 import json
-phone_book = {'Vadym':'0665699589'}
+try:
+    with open('Newj.JSON', 'r+') as handle:
+        phone_book = json.load(handle)
+except:
+    with open('Newj.JSON', 'w+') as handle:
+        phone_book = {}
 while True:
     enter = input('''You can see all contacts(stats), all names(list), someone\'s number(show) or (add), (delete) contacts.
     Enter your command: ''')
     if enter == 'add':
         name = input('Enter name to add: ')
         phone = input('Enter phone to add: ')
-        with open('Newj.JSON', 'r+') as handle:
-            js = json.load(handle)
-            phone_book = js.setdefault(name, phone)
+        phone_book.setdefault(name, phone)
         with open('Newj.JSON', 'w') as f:
-            json.dump(js, f)
+            json.dump(phone_book, f)
     elif enter == 'stats':
-        with open('Newj.JSON', 'r+') as handle:
-            js = json.load(handle)
-            print(js.values())
+            print(phone_book.values())
     elif enter == 'delete':
         delete = input('Enter name you want to delete: ')
-        with open('Newj.JSON', 'r+') as handle:
-            js = json.load(handle)
-        if delete in js:
-            del js[delete]
+        if delete in phone_book:
+            del phone_book[delete]
             with open('Newj.JSON', 'w') as f:
-                json.dump(js, f)
+                json.dump(phone_book, f)
             print(f'Contact {delete} was successfully deleted')
         else:
             print('Sorry, there is no such contact here')
     elif enter == 'list':
-        with open('Newj.JSON', 'r+') as handle:
-            js = json.load(handle)
-        print(js.keys())
+        print(phone_book.keys())
     elif enter == 'show':
         show = input('Find your contact by name: ')
-        with open('Newj.JSON', 'r+') as handle:
-            js = json.load(handle)
-        print(js.get(show, 'Sorry, there is no such contact here'))
+        print(phone_book.get(show, 'Sorry, there is no such contact here'))
     else:
         print('You entered something unexpected. Try again')
 # Ваша телефонна книга, що до цього містилася в dict, має зберігатися у вигляді тексту в JSON форматі.
@@ -44,33 +39,42 @@ while True:
 # і JSON string в dict (при читанні із файлу). Файл слід оновлювати після кожної успішної операції add або delete.
 #
 # 2. Написати декоратор, який буде записувати в файл назву функції, яку він декорує, і писати час її виклику.
-import time
-def my_decorator(func):
-    def deco_func(*args, **kwargs):
-        f = func(*args, **kwargs)
-        with open('New_txt.TXT', 'a+') as file:
-            n = func.__name__
-            t = time.strftime("%H:%M:%S")
-            note = f'Time of call function, {n}, is, {t}'
-            file.write(note+'\n')
-        return f
-    return deco_func
-@my_decorator
-def my_func(par):
-  print(par**par)
-my_func(3)
+# import time
+# def my_decorator(func):
+#     def deco_func(*args, **kwargs):
+#         f = func(*args, **kwargs)
+#         with open('New_txt.TXT', 'a+') as file:
+#             n = func.__name__
+#             t = time.strftime("%H:%M:%S")
+#             note = f'Time of call function, {n}, is, {t}'
+#             file.write(note+'\n')
+#         return f
+#     return deco_func
+# @my_decorator
+# def my_func(par):
+#   print(par**par)
+# my_func(3)
 # 3. В попередньо написаний кастомний Exception додати запис помилки і час її виникнення у файл.
-import time
-class MyCustomException(Exception):
-    def __init__(self, message = 'Custom exception is occured at '):
-        self.message = message
-        with open('error_file.TXT', 'a+') as erfile:
-            t = time.strftime("%H:%M:%S")
-            ertext = str(message + t + '\n')
-            erfile.write(ertext)
-try:
-    a=10/2
-    if a == 5:
-        raise MyCustomException
-except MyCustomException:
-    print("Check some error in file 'error_file.TXT'")
+# import time
+# class MyCustomException(Exception):
+#     def __init__(self, message = 'Custom exception is occured at '):
+#         self.message = message
+#         with open('error_file.TXT', 'a+') as erfile:
+#             t = time.strftime("%H:%M:%S")
+#             ertext = str(message + t + '\n')
+#             erfile.write(ertext)
+# try:
+#     a=10/2
+#     if a == 5:
+#         raise MyCustomException
+# except MyCustomException:
+#     print("Check some error in file 'error_file.TXT'")
+#
+
+# with open('Newj.JSON', 'r') as file:
+#     phone_book = json.load(file)
+# name = input('Enter name to add: ')
+# phone = input('Enter phone to add: ')
+# phone_book.setdefault(name, phone)
+# with open('Newj.JSON', 'w') as f:
+#     json.dump(phone_book, f)
